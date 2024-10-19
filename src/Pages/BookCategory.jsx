8d8828470b85b77a0e863c654bd98a4b57ac4c42
Sobuz_Nub <khanshobuz99@gmail.com>
2024-10-19 17:56:34 +0600

    resolve

--- src/Pages/BookCategory.jsx
+++ src/Pages/BookCategory.jsx
@@ -17,7 +17,7 @@ const BookCategory = () => {
     return (
         <>
         <Helmet>
-            <title>Book Category ||</title>
+            <title>| Book Category |</title>
         </Helmet>
             <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 my-12 max-w-7xl gap-5 mx-auto">
                 {
