1cd3abd45447227b6db1018697308bbb243465e5
Sobuz_Nub <khanshobuz99@gmail.com>
2024-10-19 18:19:04 +0600

    hover scale added to all card

--- src/Pages/Home/Home/CategorySubCard.jsx
+++ src/Pages/Home/Home/CategorySubCard.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 
 const CategorySubCard = ({ cate }) => {
-    const { _id, image, name, category } = cate;
+    const { image, name, category } = cate;
     return (
         <div>
             <Link to={`/books/${category}`}>
-                <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+                <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900 border-2 transition hover:scale-105 hover:border-purple-600">
                     <img src={image} alt="" className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
                     <div className="mt-6 mb-2">
                         <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-600">{name}</span>
